307f10e3051227c379fcc3e9bc093136a0e91dd9
GannaChernyshova <anna.chernyshova91@gmail.com>
2017-07-13 00:07:58 +0300

    added comments to Calculate class
    общее незначительное замечание, классы желательно более четко называть. Вот по названию ArrayElemLen не особо понятно, что в нем может быть.

--- Calculate/src/main/java/calculate/Calculate.java
+++ Calculate/src/main/java/calculate/Calculate.java
@@ -4,7 +4,7 @@ package calculate;
  * Created by K_PC-S on 26.06.2017.
  */
 public class Calculate {
-
+//логику класса Loop можно сюда вынести. Loop по сути не нужен
     public static void main(String[] args) {
 
         Loop l = new Loop();
